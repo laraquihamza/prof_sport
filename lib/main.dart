9a84c280bbde978de5eade18377811c101bdeea3
Hamza Laraqui Housseini <laraquihamza2703@gmail.com>
2021-06-30 10:10:59 +0100

    adding firebase packages for authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -47,7 +51,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-
+  Color col_main= Colors.blueAccent;
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -65,36 +69,49 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/coachinow.png"),
-            TextField(keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(prefixIcon: Icon(Icons.email),
-            hintText: "E-mail"),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(prefixIcon: Icon(Icons.lock),
-                  hintText: "Password"),
-            )
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child:        Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("assets/coachinow.png"),
+              TextField(keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(prefixIcon: Icon(Icons.email),
+                    hintText: "E-mail"),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(prefixIcon: Icon(Icons.lock),
+                    hintText: "Password"),
+              ),
+              MaterialButton(
+                minWidth: MediaQuery.of(context).size.width,
+                  onPressed: (){
+              },
+                color: col_main,
+                child: Text("Connexion"),
+              ),
+              Text("Mot de passe oublié ?",
+              style: TextStyle(color: Colors.black,decoration: TextDecoration.underline),
+              )
+            ],
+          ),
 
-          ],
-        ),
+        )
       ),
     );
   }
