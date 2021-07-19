bd749bea38bc1574b416d72ebd9b2b5a2a07767c
Hamza Laraqui Housseini <laraquihamza2703@gmail.com>
2021-07-19 15:04:37 +0100

    UI BOOKING

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -8,7 +8,7 @@ import 'Signup_Client.dart';
 import 'Signup_Coach.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 class Welcome extends StatefulWidget {
   final String title;
   final Client client;
@@ -30,39 +30,10 @@ class _Welcome extends State<Welcome> {
       ),
       body: Center(
         child: Column(children: [
-          Text(widget.client.email),
-          Text(widget.client.lastname),
-          Text(widget.client.firstname),
-          Text(widget.client.address),
-          Text(widget.client.city),
-          Text(widget.client.birthdate.toString()),
-          Text(widget.client.phone),
-          RaisedButton(
-              child: Text("Upload Image"),
-              onPressed: () async {
-                print("jojo");
-                String file =
-                    (await FilePicker.platform.pickFiles())!.files.single.path!;
-                Auth().UploadDocument(file);
-                print("jojo" + file);
-                /*String url=await Auth().downloadURL(file);
-                  print(url);
-                  print("jojojojo"+file);*/
-              }),
-          MaterialButton(
-            onPressed: () {
-              Auth().signOut();
-              Navigator.pop(context);
-            },
-            child: Text("Déconnexion"),
-          ),
-          Text("documents"),
-          FutureBuilder(
-              future: list_documents(),
-              builder: (context, snapshot) {
-                list_documents();
-                return Column(children: docs);
-              })
+          SfCalendar(
+          view: CalendarView.week,
+          monthViewSettings: MonthViewSettings(showAgenda: true),
+        )
         ]),
       ),
     );
