7067a59e41ee9aa3a8d9b4ac9d76600c672467ed
Hamza Laraqui Housseini <laraquihamza2703@gmail.com>
2021-07-29 23:36:17 +0100

    remove red screen on search

--- lib/reservations_client.dart
+++ lib/reservations_client.dart
@@ -7,6 +7,7 @@ import 'package:prof_sport/models/Client.dart';
 import 'package:prof_sport/models/Coach.dart';
 import 'package:prof_sport/models/ReservationService.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'models/Reservation.dart';
 class ListeDemande extends StatefulWidget {
@@ -34,101 +35,111 @@ Future<Null> get_url(String path) async{
     return Scaffold(
         appBar: custom_appbar(widget.title, context),
         body:
-        Column(
-            children: [
-              Text("Bonjour Client"),
-              StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance.collection("reservations").
-                  where("idClient",isEqualTo: widget.client.uid).snapshots(),
-                  builder: (context,snapshot) {
-                    return snapshot.data==null ? Text("null"):ListView.builder(
-                      shrinkWrap: true,
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context,index){
-                        DateTime dateDebut=snapshot.data!.docs[index]["dateDebut"].toDate();
-                        return StreamBuilder<QuerySnapshot>(
-                          stream: FirebaseFirestore.instance.collection("coaches").
-                          where("id",isEqualTo: snapshot.data!.docs[index]["idCoach"]).snapshots(),
-                          builder: (context,snap){
-                            coach=Coach(snap.data!.docs[0]["id"], "", snap.data!.docs[0]["birthdate"].toDate(),
-                                "", snap.data!.docs[0]["city"], snap.data!.docs[0]["address"],
-                                snap.data!.docs[0]["firstname"], snap.data!.docs[0]["lastname"],
-                                snap.data!.docs[0]["phone"],
-                                snap.data!.docs[0]["picture"],
-                                snap.data!.docs[0]["price"],snap.data!.docs[0]["sport"]);
-                                return
-                              Row(children:[
-                                StreamBuilder(
-                                    stream: FirebaseStorage.instance.ref(snap.data!.docs[0]["picture"]).getDownloadURL().asStream(),
-                                    builder: (context,snap2){
-                                  return                                 Container(width: 40,height: 40,decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      image: DecorationImage(
+                    StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance.collection("reservations").
+                        where("idClient",isEqualTo: widget.client.uid).snapshots(),
+                        builder: (context,snapshot) {
+                          return snapshot.data==null ? Text(""):ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: snapshot.data?.docs.length,
+                              itemBuilder: (context,index){
+                                DateTime dateDebut=snapshot.data?.docs[index]["dateDebut"].toDate();
+                                return StreamBuilder<QuerySnapshot>(
+                                  stream: FirebaseFirestore.instance.collection("coaches").
+                                  where("id",isEqualTo: snapshot.data?.docs[index]["idCoach"]).snapshots(),
+                                  builder: (context,snap){
+                                    coach=Coach(snap.data?.docs[0]["id"], "", snap.data?.docs[0]["birthdate"].toDate(),
+                                        "", snap.data?.docs[0]["city"], snap.data?.docs[0]["address"],
+                                        snap.data?.docs[0]["firstname"], snap.data?.docs[0]["lastname"],
+                                        snap.data?.docs[0]["phone"],
+                                        snap.data?.docs[0]["picture"],
+                                        snap.data?.docs[0]["price"],snap.data?.docs[0]["sport"]);
+                                    return
+                                      snapshot.data==null ? Text(""):Row(children:[
+                                        StreamBuilder(
+                                            stream: FirebaseStorage.instance.ref(snap.data?.docs[0]["picture"]).getDownloadURL().asStream(),
+                                            builder: (context,snap2){
+                                              return                     snap2.data==null ? Text(""):            Container(width: 40,height: 40,decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  image: DecorationImage(
 
-                                          image: NetworkImage(snap2.data!.toString())
-                                      )
-                                  ),);
+                                                      image: NetworkImage(snap2.data.toString())
+                                                  )
+                                              ),);
 
-                                }),
-                                SizedBox(width: 5,),
-                                Text("${coach.firstname}\n${coach.lastname}"),
-                                SizedBox(width: 5,),
-                                Text("${dateDebut.day}/${dateDebut.month}/${dateDebut.year} \n ${dateDebut.hour}:${dateDebut.minute}"),
-                                SizedBox(width: 5,),
-                                Text("${snapshot.data!.docs[index]["duration"]}h"),
+                                            }),
+                                        SizedBox(width: 5,),
+                                        Text("${coach.firstname}\n${coach.lastname}"),
+                                        SizedBox(width: 5,),
+                                        Text("${dateDebut.day}/${dateDebut.month}/${dateDebut.year} \n ${dateDebut.hour}:${dateDebut.minute}"),
+                                        SizedBox(width: 5,),
+                                        Text("${snapshot.data?.docs[index]["duration"]}h"),
 
-                                Spacer(),
-                                snapshot.data!.docs[index]["isConfirmed"] == false ?
-                                Wrap( children:
-                                [
-                                  ElevatedButton(onPressed: ()
-                                  {
+                                        Spacer(),
+                                        snapshot.data?.docs[index]["isConfirmed"] == false ?
+                                        Wrap( children:
+                                        [
+                                          ElevatedButton(onPressed: ()
+                                          {
+
+                                          },
+                                              child: Text("En Attente")),
+                                          SizedBox(width: 5,),
+                                          ElevatedButton(onPressed: ()async
+                                          {
+                                            ReservationService().refus_reservation(Reservation(id:snapshot.data?.docs[index]["id"],
+                                                idcoach: snapshot.data?.docs[index]["idCoach"],idclient: snapshot.data?.docs[index]["idClient"],
+                                                duration: snapshot.data?.docs[index]["duration"],
+                                                isConfirmed: snapshot.data?.docs[index]["isConfirmed"],
+                                                dateDebut:  snapshot.data?.docs[index]["dateDebut"].toDate()
+                                            ));
+                                          },
+                                            child: Text("Annuler"),
+                                            style: ElevatedButton.styleFrom(primary: Colors.red),
+
+                                          ),
+
+                                        ],)  :
+                                        Wrap(
+                                          children:[
+                                            ElevatedButton(
+                                              onPressed: ()
+                                              {
+                                                print("validé");
+
+                                              },
+                                              child: Text("Validé"),
+                                              style: ElevatedButton.styleFrom(primary: Colors.green),
+
+                                            ),
+                                            SizedBox(width: 5,),
+
+                                            ElevatedButton(
+                                              onPressed: () async
+                                              {
+                                                await launch("tel:"+snap.data?.docs[0]["phone"]) ;
+                                              },
+                                              child: Icon(Icons.phone, color: Colors.white,),
+                                              style: ElevatedButton.styleFrom(primary: Colors.green),
+                                            )
+                                          ],
+
+                                        ),
+
+
+                                      ]);
 
                                   },
-                                      child: Text("En Attente")),
-                                  SizedBox(width: 5,),
-                                  ElevatedButton(onPressed: ()async
-                                  {
-                                    ReservationService().refus_reservation(Reservation(id:snapshot.data!.docs[index]["id"],
-                                        idcoach: snapshot.data!.docs[index]["idCoach"],idclient: snapshot.data!.docs[index]["idClient"],
-                                        duration: snapshot.data!.docs[index]["duration"],
-                                        isConfirmed: snapshot.data!.docs[index]["isConfirmed"],
-                                        dateDebut:  snapshot.data!.docs[index]["dateDebut"].toDate()
-                                    ));
-                                  },
-                                      child: Text("Annuler"),
-                                    style: ElevatedButton.styleFrom(primary: Colors.red),
 
-                                  ),
+                                );
+                                print("urlclient:$url");
 
-                                ],)  :
-                                ElevatedButton(
-                                    onPressed: ()
-                                    {
-                                      print("validé");
-
-                                    },
-                                    child: Text("Validé"),
-                                  style: ElevatedButton.styleFrom(primary: Colors.green),
-
-                                ),
+                              });
+                        }
+                    )
+              );
 
 
-                              ]);
-
-                          },
-
-                        );
-                          print("urlclient:$url");
-
-                        });
-                  }
-              )
-            ]
-        )
-
-
-    );
   }
 }
 
--- lib/welcome_coach.dart
+++ lib/welcome_coach.dart
@@ -6,6 +6,7 @@ import 'package:prof_sport/models/Client.dart';
 import 'package:prof_sport/models/Coach.dart';
 import 'package:prof_sport/models/ReservationService.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'CustomAppBar.dart';
 import 'models/Reservation.dart';
@@ -42,38 +43,35 @@ class _Welcome_Coach extends State<Welcome_Coach> {
     return Scaffold(
         appBar: custom_appbar("Réserver ", context),
     body:
-    Column(
-    children: [
-    Text("Bonjour ${widget.coach.firstname} ${widget.coach.lastname}"),
     StreamBuilder<QuerySnapshot>(
     stream: FirebaseFirestore.instance.collection("reservations").
     where("idCoach",isEqualTo: widget.coach.uid).snapshots(),
     builder: (context,snapshot) {
     return snapshot.data==null ? Text("null"):ListView.builder(
     shrinkWrap: true,
-    itemCount: snapshot.data!.docs.length,
+    itemCount: snapshot.data?.docs.length,
     itemBuilder: (context,index){
-    return StreamBuilder<QuerySnapshot>(
+    return snapshot.data==null?Text(""):StreamBuilder<QuerySnapshot>(
     stream: FirebaseFirestore.instance.collection("users").
-    where("id",isEqualTo: snapshot.data!.docs[index]["idClient"]).snapshots(),
+    where("id",isEqualTo: snapshot.data?.docs[index]["idClient"]).snapshots(),
     builder: (context,snap){
-    client=Client(snap.data!.docs[0]["id"], "", snap.data!.docs[0]["birthdate"].toDate(),
-    "", snap.data!.docs[0]["city"], snap.data!.docs[0]["address"],
-    snap.data!.docs[0]["firstname"], snap.data!.docs[0]["lastname"],
-    snap.data!.docs[0]["phone"],
-    snap.data!.docs[0]["picture"],
+    client=Client(snap.data?.docs[0]["id"], "", snap.data?.docs[0]["birthdate"].toDate(),
+    "", snap.data?.docs[0]["city"], snap.data?.docs[0]["address"],
+    snap.data?.docs[0]["firstname"], snap.data?.docs[0]["lastname"],
+    snap.data?.docs[0]["phone"],
+    snap.data?.docs[0]["picture"],
     );
-    DateTime dateDebut=snapshot.data!.docs[index]["dateDebut"].toDate();
+    DateTime dateDebut=snapshot.data?.docs[index]["dateDebut"].toDate();
     return
     Row(children:[
     StreamBuilder(
-    stream: FirebaseStorage.instance.ref(snap.data!.docs[0]["picture"]).getDownloadURL().asStream(),
+    stream: FirebaseStorage.instance.ref(snap.data?.docs[0]["picture"]).getDownloadURL().asStream(),
     builder: (context,snap2){
     return                                 Container(width: 40,height: 40,decoration: BoxDecoration(
     shape: BoxShape.circle,
     image: DecorationImage(
 
-    image: NetworkImage(snap2.data!.toString())
+    image: NetworkImage(snap2.data.toString())
     )
     ),);
 
@@ -83,30 +81,30 @@ class _Welcome_Coach extends State<Welcome_Coach> {
       SizedBox(width: 5,),
       Text("${dateDebut.day}/${dateDebut.month}/${dateDebut.year} \n ${dateDebut.hour}:${dateDebut.minute}"),
       SizedBox(width: 5,),
-      Text("${snapshot.data!.docs[index]["duration"]}h"),
+      Text("${snapshot.data?.docs[index]["duration"]}h"),
 
       Spacer(),
-    snapshot.data!.docs[index]["isConfirmed"] == false ?
+    snapshot.data?.docs[index]["isConfirmed"] == false ?
     Wrap( children:
     [
     ElevatedButton(onPressed: ()
     {
-        ReservationService().confirm_reservation(Reservation(id:snapshot.data!.docs[index]["id"],
-            idcoach: snapshot.data!.docs[index]["idCoach"],idclient: snapshot.data!.docs[index]["idClient"],
-            duration: snapshot.data!.docs[index]["duration"],
-            isConfirmed: snapshot.data!.docs[index]["isConfirmed"],
-            dateDebut:  snapshot.data!.docs[index]["dateDebut"].toDate()
+        ReservationService().confirm_reservation(Reservation(id:snapshot.data?.docs[index]["id"],
+            idcoach: snapshot.data?.docs[index]["idCoach"],idclient: snapshot.data?.docs[index]["idClient"],
+            duration: snapshot.data?.docs[index]["duration"],
+            isConfirmed: snapshot.data?.docs[index]["isConfirmed"],
+            dateDebut:  snapshot.data?.docs[index]["dateDebut"].toDate()
         ));
     },
     child: Text("Valider")),
     SizedBox(width: 5,),
     ElevatedButton(onPressed: ()async
     {
-    ReservationService().refus_reservation(Reservation(id:snapshot.data!.docs[index]["id"],
-    idcoach: snapshot.data!.docs[index]["idCoach"],idclient: snapshot.data!.docs[index]["idClient"],
-    duration: snapshot.data!.docs[index]["duration"],
-    isConfirmed: snapshot.data!.docs[index]["isConfirmed"],
-    dateDebut:  snapshot.data!.docs[index]["dateDebut"].toDate()
+    ReservationService().refus_reservation(Reservation(id:snapshot.data?.docs[index]["id"],
+    idcoach: snapshot.data?.docs[index]["idCoach"],idclient: snapshot.data?.docs[index]["idClient"],
+    duration: snapshot.data?.docs[index]["duration"],
+    isConfirmed: snapshot.data?.docs[index]["isConfirmed"],
+    dateDebut:  snapshot.data?.docs[index]["dateDebut"].toDate()
     ));
     },
     child: Text("Refuser")
@@ -116,33 +114,45 @@ class _Welcome_Coach extends State<Welcome_Coach> {
 
     )
     ],)  :
-    ElevatedButton(
-    onPressed: ()
-    {
-    print("validé");
+    Wrap(
+      children:[
+        ElevatedButton(
+          onPressed: ()
+          {
+            print("validé");
 
-    },
-    child: Text("Validé"),
-      style: ElevatedButton.styleFrom(primary: Colors.green),
+          },
+          child: Text("Validé"),
+          style: ElevatedButton.styleFrom(primary: Colors.green),
 
-      ),
+        ),
+        SizedBox(width: 5,),
+
+        ElevatedButton(
+          onPressed: () async
+          {
+            await launch("tel:"+snap.data?.docs[0]["phone"]) ;
+          },
+          child: Icon(Icons.phone, color: Colors.white,),
+          style: ElevatedButton.styleFrom(primary: Colors.green),
+        )
+      ],
+
+    ),
 
 
     ]);
 
-    },
-
+    }
     );
-    print("urlclient:$url");
+
 
     });
     }
     )
-    ]
-    )
-
-
     );
+
+
   }
 
 
